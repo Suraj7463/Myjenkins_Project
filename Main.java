dc149076fbccadbc68b423298cd892b3b7d6428f
Suraj7463 <surajdudhabhate85@gmail.com>
2025-05-02 12:26:57 +0530

    add new file

--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ public class Main{
     public static void main(String args[]){
         System.out.println("i am here");
         System.out.println("i am good here");
+
+        Student std=new Student();
+        std.Massage();
     }
 }
---
+++ Student.java
@@ -0,0 +1,8 @@
+public class Student{
+    public Student(){
+        System.out.println("create object");
+    }
+    public void Massage(){
+        System.out.println("where are you from bro");
+    }
+}
